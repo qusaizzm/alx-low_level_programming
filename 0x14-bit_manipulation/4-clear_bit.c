9f57f2023eb4f5e032fb050f79493f8cf5ad77fa
Qusai Alsimat <qusai_zzm@outlook.com>
2023-10-26 13:24:48 +0300

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,19 +1,13 @@
 #include "main.h"
-#include <stdio.h>
  
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int i;
-	unsigned int hold;
 
-	if (index > 64)
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	hold = index;
-	for (i = 1; hold > 0; i *= 2, hold--)
-		;
-
-	if ((*n >> index) & 1)
-		*n -= i;
+	i = ~(1 << index);
+	*n = *n & i;
 
 	return (1);
 }

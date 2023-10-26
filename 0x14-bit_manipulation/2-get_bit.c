c85dae553adc19f2fe97c91108ff3e562b9457cc
Qusai Alsimat <qusai_zzm@outlook.com>
2023-10-26 13:23:55 +0300

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,14 +1,15 @@
 #include "main.h"
-#include <stdio.h>
  
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int hold;
+	unsigned long int div, res;
 
-	if (index > 64)
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
+	div = 1 << index;
+	res = n & div;
+	if (res == div)
+		return (1);
 
-	hold = n >> index;
-
-	return (hold & 1);
+	return (0);
 }
